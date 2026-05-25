368ff00a6537594e7a64ba514c536ccf9fb7469c
vargab3 <146070587+vargab3@users.noreply.github.com>
2026-05-25 22:50:39 +0200

    Add files via upload

---
+++ CategoriesController.cs
@@ -0,0 +1,156 @@
+{
+  "Version": 1,
+  "WorkspaceRootPath": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\",
+  "Documents": [
+    {
+      "AbsoluteMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|c:\\users\\kerenyir\\source\\repos\\kerenyi_r\u00F3bert_n\u00E1ndor_backend\\kerenyi_r\u00F3bert_n\u00E1ndor\\controllers\\categoriescontroller.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}",
+      "RelativeMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|solutionrelative:kerenyi_r\u00F3bert_n\u00E1ndor\\controllers\\categoriescontroller.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}"
+    },
+    {
+      "AbsoluteMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|c:\\users\\kerenyir\\source\\repos\\kerenyi_r\u00F3bert_n\u00E1ndor_backend\\kerenyi_r\u00F3bert_n\u00E1ndor\\controllers\\bookscontroller.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}",
+      "RelativeMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|solutionrelative:kerenyi_r\u00F3bert_n\u00E1ndor\\controllers\\bookscontroller.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}"
+    },
+    {
+      "AbsoluteMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|c:\\users\\kerenyir\\source\\repos\\kerenyi_r\u00F3bert_n\u00E1ndor_backend\\kerenyi_r\u00F3bert_n\u00E1ndor\\program.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}",
+      "RelativeMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|solutionrelative:kerenyi_r\u00F3bert_n\u00E1ndor\\program.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}"
+    },
+    {
+      "AbsoluteMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|c:\\users\\kerenyir\\source\\repos\\kerenyi_r\u00F3bert_n\u00E1ndor_backend\\kerenyi_r\u00F3bert_n\u00E1ndor\\controllers\\authorscontroller.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}",
+      "RelativeMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|solutionrelative:kerenyi_r\u00F3bert_n\u00E1ndor\\controllers\\authorscontroller.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}"
+    },
+    {
+      "AbsoluteMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|c:\\users\\kerenyir\\source\\repos\\kerenyi_r\u00F3bert_n\u00E1ndor_backend\\kerenyi_r\u00F3bert_n\u00E1ndor\\models\\category.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}",
+      "RelativeMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|solutionrelative:kerenyi_r\u00F3bert_n\u00E1ndor\\models\\category.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}"
+    },
+    {
+      "AbsoluteMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|c:\\users\\kerenyir\\source\\repos\\kerenyi_r\u00F3bert_n\u00E1ndor_backend\\kerenyi_r\u00F3bert_n\u00E1ndor\\models\\librarydbcontext.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}",
+      "RelativeMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|solutionrelative:kerenyi_r\u00F3bert_n\u00E1ndor\\models\\librarydbcontext.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}"
+    },
+    {
+      "AbsoluteMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|c:\\users\\kerenyir\\source\\repos\\kerenyi_r\u00F3bert_n\u00E1ndor_backend\\kerenyi_r\u00F3bert_n\u00E1ndor\\models\\author.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}",
+      "RelativeMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|solutionrelative:kerenyi_r\u00F3bert_n\u00E1ndor\\models\\author.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}"
+    },
+    {
+      "AbsoluteMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|c:\\users\\kerenyir\\source\\repos\\kerenyi_r\u00F3bert_n\u00E1ndor_backend\\kerenyi_r\u00F3bert_n\u00E1ndor\\models\\book.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}",
+      "RelativeMoniker": "D:0:0:{6414D97E-F9E3-4A77-8BB7-566EFB312D02}|Kerenyi_R\u00F3bert_N\u00E1ndor\\Kerenyi_R\u00F3bert_N\u00E1ndor.csproj|solutionrelative:kerenyi_r\u00F3bert_n\u00E1ndor\\models\\book.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}"
+    }
+  ],
+  "DocumentGroupContainers": [
+    {
+      "Orientation": 0,
+      "VerticalTabListWidth": 256,
+      "DocumentGroups": [
+        {
+          "DockedWidth": 200,
+          "SelectedChildIndex": 1,
+          "Children": [
+            {
+              "$type": "Document",
+              "DocumentIndex": 4,
+              "Title": "Category.cs",
+              "DocumentMoniker": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\Category.cs",
+              "RelativeDocumentMoniker": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\Category.cs",
+              "ToolTip": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\Category.cs",
+              "RelativeToolTip": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\Category.cs",
+              "ViewState": "AgIAAAAAAAAAAAAAAAAAAAUAAAAAAAAAAAAAAA==",
+              "Icon": "ae27a6b0-e345-4288-96df-5eaf394ee369.000738|",
+              "WhenOpened": "2026-02-20T08:57:56.196Z",
+              "EditorCaption": ""
+            },
+            {
+              "$type": "Document",
+              "DocumentIndex": 0,
+              "Title": "CategoriesController.cs",
+              "DocumentMoniker": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Controllers\\CategoriesController.cs",
+              "RelativeDocumentMoniker": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Controllers\\CategoriesController.cs",
+              "ToolTip": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Controllers\\CategoriesController.cs",
+              "RelativeToolTip": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Controllers\\CategoriesController.cs",
+              "ViewState": "AgIAAAkAAAAAAAAAAAAAABMAAABvAAAAAAAAAA==",
+              "Icon": "ae27a6b0-e345-4288-96df-5eaf394ee369.000738|",
+              "WhenOpened": "2026-02-20T08:43:57.133Z",
+              "EditorCaption": ""
+            },
+            {
+              "$type": "Document",
+              "DocumentIndex": 1,
+              "Title": "BooksController.cs",
+              "DocumentMoniker": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Controllers\\BooksController.cs",
+              "RelativeDocumentMoniker": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Controllers\\BooksController.cs",
+              "ToolTip": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Controllers\\BooksController.cs",
+              "RelativeToolTip": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Controllers\\BooksController.cs",
+              "ViewState": "AgIAAAAAAAAAAAAAAAAAACQAAAAbAAAAAAAAAA==",
+              "Icon": "ae27a6b0-e345-4288-96df-5eaf394ee369.000738|",
+              "WhenOpened": "2026-02-20T08:41:00.808Z",
+              "EditorCaption": ""
+            },
+            {
+              "$type": "Document",
+              "DocumentIndex": 3,
+              "Title": "AuthorsController.cs",
+              "DocumentMoniker": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Controllers\\AuthorsController.cs",
+              "RelativeDocumentMoniker": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Controllers\\AuthorsController.cs",
+              "ToolTip": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Controllers\\AuthorsController.cs",
+              "RelativeToolTip": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Controllers\\AuthorsController.cs",
+              "ViewState": "AgIAABgAAAAAAAAAAAAiwCcAAAAoAAAAAAAAAA==",
+              "Icon": "ae27a6b0-e345-4288-96df-5eaf394ee369.000738|",
+              "WhenOpened": "2026-02-20T08:18:57.102Z",
+              "EditorCaption": ""
+            },
+            {
+              "$type": "Document",
+              "DocumentIndex": 2,
+              "Title": "Program.cs",
+              "DocumentMoniker": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Program.cs",
+              "RelativeDocumentMoniker": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Program.cs",
+              "ToolTip": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Program.cs",
+              "RelativeToolTip": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Program.cs",
+              "ViewState": "AgIAAAAAAAAAAAAAAAAAAAcAAAAWAAAAAAAAAA==",
+              "Icon": "ae27a6b0-e345-4288-96df-5eaf394ee369.000738|",
+              "WhenOpened": "2026-02-20T08:17:36.869Z",
+              "EditorCaption": ""
+            },
+            {
+              "$type": "Document",
+              "DocumentIndex": 6,
+              "Title": "Author.cs",
+              "DocumentMoniker": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\Author.cs",
+              "RelativeDocumentMoniker": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\Author.cs",
+              "ToolTip": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\Author.cs",
+              "RelativeToolTip": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\Author.cs",
+              "ViewState": "AgIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==",
+              "Icon": "ae27a6b0-e345-4288-96df-5eaf394ee369.000738|",
+              "WhenOpened": "2026-02-20T08:16:25.492Z",
+              "EditorCaption": ""
+            },
+            {
+              "$type": "Document",
+              "DocumentIndex": 7,
+              "Title": "Book.cs",
+              "DocumentMoniker": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\Book.cs",
+              "RelativeDocumentMoniker": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\Book.cs",
+              "ToolTip": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\Book.cs",
+              "RelativeToolTip": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\Book.cs",
+              "ViewState": "AgIAAAkAAAAAAAAAAAA1wBMAAAAcAAAAAAAAAA==",
+              "Icon": "ae27a6b0-e345-4288-96df-5eaf394ee369.000738|",
+              "WhenOpened": "2026-02-20T08:15:22.152Z",
+              "EditorCaption": ""
+            },
+            {
+              "$type": "Document",
+              "DocumentIndex": 5,
+              "Title": "LibrarydbContext.cs",
+              "DocumentMoniker": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\LibrarydbContext.cs",
+              "RelativeDocumentMoniker": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\LibrarydbContext.cs",
+              "ToolTip": "C:\\Users\\kerenyir\\source\\repos\\Kerenyi_R\u00F3bert_N\u00E1ndor_Backend\\Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\LibrarydbContext.cs",
+              "RelativeToolTip": "Kerenyi_R\u00F3bert_N\u00E1ndor\\Models\\LibrarydbContext.cs",
+              "ViewState": "AgIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==",
+              "Icon": "ae27a6b0-e345-4288-96df-5eaf394ee369.000738|",
+              "WhenOpened": "2026-02-20T08:15:00.307Z",
+              "EditorCaption": ""
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
